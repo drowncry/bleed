aac680281756b36aa6135163e58589e8fd57d943
drowncry <uoeonwm@proton.me>
2026-05-11 05:14:37 -0700

    Update command.js

--- handlers/command.js
+++ handlers/command.js
@@ -1,28 +1,38 @@
 const { readdirSync } = require("fs");
-
-const ascii = require("ascii-table");
-
-let table = new ascii("Commands");
-table.setHeading("Command", "Load status");
+const Ascii = require("ascii-table");
+let table = new Ascii("Comandos");
+table.setHeading("Comando", "Estado");
 
 module.exports = (client) => {
+    // Estas son las carpetas que el bot va a leer ahora
+    const categorias = [
+        "configuration", 
+        "fun", 
+        "information", 
+        "lastfm", 
+        "moderation", 
+        "owner", 
+        "utility"
+    ];
 
-    readdirSync("./commands/").forEach(dir => {
-        const commands = readdirSync(`./commands/${dir}/`).filter(file => file.endsWith(".js"));
-
+    categorias.forEach(dir => {
+        const commands = readdirSync(`./${dir}/`).filter(file => file.endsWith(".js"));
+    
         for (let file of commands) {
-            let pull = require(`../commands/${dir}/${file}`);
-
+            let pull = require(`../${dir}/${file}`);
+    
             if (pull.name) {
                 client.commands.set(pull.name, pull);
                 table.addRow(file, '✅');
             } else {
-                table.addRow(file, `❌  -> missing a help.name, or help.name is not a string.`);
+                table.addRow(file, `❌ -> falta help.name`);
                 continue;
             }
-            if (pull.aliases && Array.isArray(pull.aliases)) pull.aliases.forEach(alias => client.aliases.set(alias, pull.name));
+    
+            if (pull.aliases && Array.isArray(pull.aliases)) 
+                pull.aliases.forEach(alias => client.aliases.set(alias, pull.name));
         }
     });
-
-    //console.log(table.toString());
+    
+    console.log(table.toString());
 }
