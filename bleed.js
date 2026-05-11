df87008e8fff68532798600698622f23042e9c6e
drowncry <uoeonwm@proton.me>
2026-05-11 04:20:30 -0700

    Update bleed.js

--- bleed.js
+++ bleed.js
@@ -1,25 +1,42 @@
-const { token, default_prefix, color } = require("./config.json");
+const http = require('http');
+// Servidor para que Render mantenga el bot encendido
+http.createServer(function (req, res) {
+  res.write('Bot is online');
+  res.end();
+}).listen(process.env.PORT || 8080);
+
+const { default_prefix, color } = require("./config.json");
+// Usamos process.env.TOKEN para que sea más seguro
+const token = process.env.TOKEN; 
 const Discord = require("discord.js");
 require("@haileybot/sanitize-role-mentions")();
+
 const client = new Discord.Client({
   disableMentions: "everyone",
   fetchAllMembers: true,
   partials: ['MESSAGE', 'REACTION']
 });
+
 const mongoose = require('mongoose')
-mongoose.connect('mongo url', {
+// Asegúrate de poner tu URL de Mongo en las variables de Render también
+mongoose.connect(process.env.MONGO_URL || 'mongo url', {
   useUnifiedTopology: true,
   useNewUrlParser: true
 }).then(console.log('connected to mongoose'))
+
 const jointocreate = require("./jointocreate");
 jointocreate(client);
+
 client.commands = new Discord.Collection();
 client.aliases = new Discord.Collection();
 client.db = require("quick.db");
+
 module.exports = client;
+
 ["command", "event"].forEach(handler => {
   require(`./handlers/${handler}`)(client);
 });
+
 Discord.Constants.DefaultOptions.ws.properties.$browser = "Discord Android"
 
 client.login(token)
